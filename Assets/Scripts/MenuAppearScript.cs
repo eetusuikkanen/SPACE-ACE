798880a1550a27b637a2735687c6ba4e549c4c59
Orgelon <mokasitte@gmail.com>
2020-12-28 22:05:01 +0200

    V1.3
    Fixed some stuff, added a pause menu and changed some möösik.

---
+++ Assets/Scripts/MenuAppearScript.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+using System.Collections;
+
+public class MenuAppearScript : MonoBehaviour
+{
+    public Canvas CanvasObject; // Assign in inspector
+    public EventSystem EventSystem; // Assign in inspector
+    public StandaloneInputModule StandaloneInputModule;
+    public QuitGame QuitGame;
+    public ChangeScene ChangeScene;
+
+
+    void Start()
+    {
+        CanvasObject = GetComponent<Canvas>();
+        //QuitGame = GetComponent<QuitGame>();
+        //ChangeScene = GetComponent<ChangeScene>();
+        StandaloneInputModule = GetComponent<StandaloneInputModule>();
+        EventSystem = GetComponent<EventSystem>();
+
+    }
+
+    void Update()
+    {
+        if (Input.GetButtonDown("Cancel"))
+        {
+            CanvasObject.enabled = !CanvasObject.enabled;
+            Time.timeScale = CanvasObject.enabled ? 0 : 1;
+            StandaloneInputModule.enabled = !StandaloneInputModule.enabled;
+            //QuitGame.enabled = !QuitGame.enabled;
+            //ChangeScene.enabled = !ChangeScene.enabled;
+           
+        }
+    }
+}
+
+
